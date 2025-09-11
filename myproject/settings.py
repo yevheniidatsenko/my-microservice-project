513f215fd95cc7bd8c878c7c249098477ad22a72
Yevhenii Datsenko <yevheniydatsenko@gmail.com>
2025-09-11 12:47:01 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'myproject'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'postgres-service'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
